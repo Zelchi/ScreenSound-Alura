2c2cdffe4780c3703c740159a265e21e059a8288
Zelchi <tininhakawaii@gmail.com>
2025-05-22 21:29:24 -0300

    Refatoração das classes de música e avaliação
    - Adicionado namespace `ScreenSound.Models` e alteradas classes para internas.
    - Implementada a classe `Avaliacao` com método estático `Parse`.
    - Modificada a classe `Banda` para usar `List<Avaliacao>` e atualizado cálculo da média.
    - Alterada a estrutura de dados em `Program.cs` para mapear bandas a objetos `Banda`.
    - Melhorada a robustez do menu com `int.TryParse`.
    - Atualizados métodos de registro de álbuns e bandas para usar novas estruturas.
    - Modificado `AvaliarUmaBanda` para utilizar a classe `Avaliacao`.

--- ScreenSound/Models/Album.cs
+++ ScreenSound/Models/Album.cs
@@ -1,10 +1,14 @@
-﻿class Album
+﻿namespace ScreenSound.Models;
+
+internal class Album
 {
+    private static int quantidadeDeAlbums = 0; // Deixando a quantidade de Albums criados como uma propiedade estática
     private List<Musica> musicas = new List<Musica>();
 
     public Album(string nome)
     {
         Nome = nome;
+        quantidadeDeAlbums++; 
     }
 
     public string Nome { get; }
@@ -24,5 +28,16 @@
             Console.WriteLine($"Música: {musica.Nome}");
         }
         Console.WriteLine($"\nPara ouvir este álbum inteiro você precisa de {DuracaoTotal}");
+    }
+
+    public static int ContadorDeObjetos => quantidadeDeAlbums;
+
+    public static void Batata()
+    {
+        Album a1 = new Album("Barões da Pisadinha Ao Vivo");
+        Album a2 = new Album("Barões da Pisadinha feat.Anitta");
+        Album a3 = new Album("Barões da Pisadinha no Free Jazz Festival");
+
+        Console.WriteLine($"Total de objetos criados: {Album.quantidadeDeAlbums}");
     }
 }
