cd962d75c17735e26cf00e3c70219e3e184f436b
Zelchi <tininhakawaii@gmail.com>
2025-06-19 11:22:55 -0300

    Add initial migration and update DbContext configuration for SQLite

--- Program/Banco/DataContext.cs
+++ Program/Banco/DataContext.cs
@@ -3,15 +3,15 @@ using ScreenSound.Modelos;
 
 namespace ScreenSound.Banco;
 
-internal class ScreenSoundContext: DbContext
+internal class ScreenSoundContext : DbContext
 {
     public DbSet<Artista> Artistas { get; set; }
     public DbSet<Musica> Musicas { get; set; }
 
-    private string connectionString = "Data Source=ScreenSound.db";
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
+        string connectionString = $"Data Source={Path.Combine(AppContext.BaseDirectory, "ScreenSound.db")}";
         optionsBuilder.UseSqlite(connectionString);
     }
 }
