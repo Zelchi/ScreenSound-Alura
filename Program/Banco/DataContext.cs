22f3a773392dc35606d18c8959802758255ebca6
Zelchi <tininhakawaii@gmail.com>
2025-06-19 01:05:28 -0300

    Update database configuration to use SQLite and upgrade Entity Framework packages

--- Program/Banco/DataContext.cs
+++ Program/Banco/DataContext.cs
@@ -8,10 +8,10 @@ internal class ScreenSoundContext: DbContext
     public DbSet<Artista> Artistas { get; set; }
     public DbSet<Musica> Musicas { get; set; }
 
-    private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+    private string connectionString = "Data Source=ScreenSound.db";
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(connectionString);
+        optionsBuilder.UseSqlite(connectionString);
     }
 }
