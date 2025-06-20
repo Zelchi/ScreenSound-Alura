ebb1c04afe3b1092489680074a8439f46e5c1bf8
Zelchi <tininhakawaii@gmail.com>
2025-06-20 00:48:47 -0300

    Add Entity Framework Core context and repository classes for Artista and Musica entities

--- ScreenSound.Biblioteca/Banco/DAL.cs
+++ ScreenSound.Biblioteca/Banco/DAL.cs
@@ -1,8 +1,8 @@
 namespace ScreenSound.Banco;
 
-internal class DAL<T> where T : class
+public class DAL<T> where T : class
 {
-    protected readonly ScreenSoundContext context;
+    private readonly ScreenSoundContext context;
 
     public DAL(ScreenSoundContext context)
     {
